c4a3a1db60be38de88c1c19a1589af4c50877d24
BelyaevMikhail <ivzh.ehb@yandex.ru>
2021-11-01 17:04:43 +0300

    PyQt_Lesson_3

--- decos.py
+++ decos.py
@@ -2,7 +2,9 @@
 
 import sys
 import logging
+# noinspection PyUnresolvedReferences
 import logs.config_server_log
+# noinspection PyUnresolvedReferences
 import logs.config_client_log
 import traceback
 import inspect
@@ -35,15 +37,15 @@ def log(func_to_log):
 
 
 # Реализация в виде класса
-class Log:
-    """Класс-декоратор"""
+"""class Log:
+    #Класс-декоратор
     def __call__(self, func_to_log):
         def log_saver(*args, **kwargs):
-            """Обертка"""
+            #Обертка
             ret = func_to_log(*args, **kwargs)
             LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
                          f'Вызов из модуля {func_to_log.__module__}. Вызов из'
                          f' функции {traceback.format_stack()[0].strip().split()[-1]}.'
                          f'Вызов из функции {inspect.stack()[1][3]}', stacklevel=2)
             return ret
-        return log_saver
+        return log_saver"""
